791d190a34d486b6c1d0e03fbf20410b95bc16da
Adivfp9 <55520235+Adivfp9@users.noreply.github.com>
2022-11-26 21:42:40 +0700

    Update Todo

--- lib/ui/todo/todo.dart
+++ lib/ui/todo/todo.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:provider/provider.dart';
 import 'package:todo_apps/data/models/todo.dart';
 import 'package:todo_apps/data/provider/todo/todo_provider.dart';
@@ -40,6 +41,17 @@ class TodoPages extends StatelessWidget {
                     addAutomaticKeepAlives: true,
                     itemBuilder: (context, idx) {
                       var todo = state.todoList[idx];
+                      final f = DateFormat('dd-MMMM-yyyy');
+
+                      DateTime starDateVal = todo.startDate != null
+                          ? DateTime.fromMillisecondsSinceEpoch(
+                              int.parse(todo.startDate!))
+                          : DateTime.now();
+                      DateTime dueDateVal = todo.dueDate != null
+                          ? DateTime.fromMillisecondsSinceEpoch(
+                              int.parse(todo.dueDate!))
+                          : DateTime.now();
+
                       return InkWell(
                         onTap: () {
                           Navigator.pushNamed(context, '/todo_detail',
@@ -48,35 +60,73 @@ class TodoPages extends StatelessWidget {
                                 todo: state.todoList[idx],
                               ));
                         },
-                        child: Card(
-                          elevation: 5.0,
-                          color: todo.isDone! ? Colors.green : Colors.white,
-                          child: Padding(
-                            padding: const EdgeInsets.all(5.0),
-                            child: ListTile(
-                              title: Text(
-                                '${todo.taskName}',
-                                style: const TextStyle(
-                                  fontSize: 18,
-                                ),
-                              ),
-                              trailing: Checkbox(
-                                value: todo.isDone,
-                                onChanged: (value) async {
-                                  state.setDone(value!);
-                                  todo.isDone = state.isCheckDone;
-                                  Todo updateTodo = Todo();
-                                  updateTodo.taskName = todo.taskName;
-                                  updateTodo.startDate = todo.startDate;
-                                  updateTodo.dueDate = todo.dueDate;
-                                  updateTodo.id = todo.id;
-                                  updateTodo.starred = false;
-                                  updateTodo.isDone = state.isCheckDone;
-                                  print(updateTodo.toJson());
-                                  print(state.isCheckDone);
+                        child: Padding(
+                          padding: const EdgeInsets.fromLTRB(4.0, 0, 4.0, 0),
+                          child: Card(
+                            elevation: 5.0,
+                            color: todo.isDone! ? Colors.green : Colors.white,
+                            child: Padding(
+                              padding: const EdgeInsets.all(15.0),
+                              child: Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceBetween,
+                                    children: [
+                                      Text(
+                                        todo.taskName!,
+                                        style: const TextStyle(
+                                            fontWeight: FontWeight.w600,
+                                            fontSize: 20.0,
+                                            color: Colors.black87),
+                                      ),
+                                      const SizedBox(
+                                        height: 5,
+                                      ),
+                                      Text(
+                                        "Start Date : ${f.format(starDateVal)}",
+                                        style: TextStyle(
+                                            fontWeight: FontWeight.normal,
+                                            fontSize: 14.0,
+                                            color: todo.isDone!
+                                                ? Colors.black
+                                                : Colors.blueGrey),
+                                      ),
+                                      const SizedBox(
+                                        height: 5,
+                                      ),
+                                      Text(
+                                        "Due Date : ${f.format(dueDateVal)}",
+                                        style: TextStyle(
+                                            fontWeight: FontWeight.normal,
+                                            fontSize: 14.0,
+                                            color: todo.isDone!
+                                                ? Colors.black
+                                                : Colors.blueGrey),
+                                      ),
+                                    ],
+                                  ),
+                                  Checkbox(
+                                    value: todo.isDone,
+                                    onChanged: (value) async {
+                                      state.setDone(value!);
+                                      todo.isDone = state.isCheckDone;
+                                      Todo updateTodo = Todo();
+                                      updateTodo.taskName = todo.taskName;
+                                      updateTodo.startDate = todo.startDate;
+                                      updateTodo.dueDate = todo.dueDate;
+                                      updateTodo.id = todo.id;
+                                      updateTodo.starred = false;
+                                      updateTodo.isDone = state.isCheckDone;
 
-                                  await state.updateTodo(updateTodo);
-                                },
+                                      await state.updateTodo(updateTodo);
+                                    },
+                                  ),
+                                ],
                               ),
                             ),
                           ),
